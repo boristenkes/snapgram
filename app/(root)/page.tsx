05ae8d599fe0a45138b87d16a9719a5d6c40a33b
Boris Tenkes <boris.tenkes.dev@gmail.com>
2024-01-29 20:56:19 +0100

    Moved right sidebar into separate component and added footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import Searchbar from '@/components/search'
 import StoryTrack from '@/components/story-track'
-import SuggestedAccounts from './suggested-accounts'
+import RightSidebar from './right-sidebar'
 
 export default function Home() {
 	return (
@@ -8,10 +7,8 @@ export default function Home() {
 			<main className='flex-1'>
 				<StoryTrack />
 			</main>
-			<aside className='position-sticky right-0 top-0 h-screen w-full max-w-sm border-l border-l-neutral-700 bg-neutral-800 px-6 py-14 hidden lg:block xl:max-w-md'>
-				<Searchbar placeholder='Search users...' />
-				<SuggestedAccounts />
-			</aside>
+
+			<RightSidebar />
 		</div>
 	)
 }
---
+++ app/(root)/right-sidebar.tsx
@@ -0,0 +1,36 @@
+import { sidebarLinks } from '@/constants'
+import SuggestedAccounts from './suggested-accounts'
+import Searchbar from '@/components/search'
+import Link from 'next/link'
+
+export default function RightSidebar() {
+	return (
+		<aside className='position-sticky right-0 top-0 h-screen w-full max-w-sm border-l border-l-neutral-700 bg-neutral-800 px-6 py-14 hidden lg:flex lg:flex-col xl:max-w-md'>
+			<Searchbar placeholder='Search users...' />
+
+			<SuggestedAccounts />
+
+			<footer className='mt-auto text-center space-y-2'>
+				<p className='text-neutral-400'>
+					&copy; {new Date().getFullYear()} Snapgram
+				</p>
+
+				<ul className='flex flex-wrap justify-center text-sm text-neutral-400'>
+					{sidebarLinks.map((link, index) => (
+						<li key={link.path}>
+							<Link
+								href={link.path}
+								className='hover:underline hover:text-neutral-500'
+							>
+								{link.title}
+							</Link>
+							{index !== sidebarLinks.length - 1 && (
+								<span className='mx-1'>&bull;</span>
+							)}
+						</li>
+					))}
+				</ul>
+			</footer>
+		</aside>
+	)
+}
