bd3b11b7d13a893b49ddf66e7996b65a80466c1d
Boris Tenkes <boris.tenkes.dev@gmail.com>
2024-06-28 23:40:31 +0200

    Fixed stories repeating on story-bubble-track

--- components/story-bubble-track/index.tsx
+++ components/story-bubble-track/index.tsx
@@ -19,23 +19,23 @@ export default async function StoryBubbleTrack() {
 		<ul className='overflow-x-auto custom-scrollbar flex items-center gap-3 sm:gap-6 px-4 py-5 mb-4 md:p-10 lg:p-16'>
 			<MyStory />
 
-			{Array.from({ length: 20 }, (_, idx) => idx).map(idx => (
+			{/* {Array.from({ length: 20 }, (_, idx) => idx).map(idx => (
 				<li
 					key={idx}
 					className='relative flex-none text-center'
 				>
 					<StoryBubble story={response.stories[0]} />
 				</li>
-			))}
+			))} */}
 
-			{/* {response.stories?.map(story => (
-					<li
-						key={story.author._id}
-						className='relative flex-none text-center'
-					>
-						<StoryBubble story={story} />
-					</li>
-				))} */}
+			{response.stories?.map(story => (
+				<li
+					key={story.author._id}
+					className='relative flex-none text-center'
+				>
+					<StoryBubble story={story} />
+				</li>
+			))}
 		</ul>
 	)
 }
